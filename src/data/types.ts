bbfebb86ee1cd08094e3e2cca85ceb5590b97802
FSB-code <formozovsb@gmail.com>
2023-12-07 20:05:53 +0300

    adds new types

---
+++ src/data/types.ts
@@ -0,0 +1,16 @@
+export interface IFunction {
+    name: string,
+    arguments: any[]
+}
+
+export interface ICondition {
+    id: string,
+    textIncludes: string, // строка которая должна содержаться в расшифрованном тексте
+    f: IFunction // вызов функция которая вернет boolean
+}
+
+export interface IRule {
+    id: string,
+    condition: ICondition[] // массив условий
+    action: IFunction // действие
+}
